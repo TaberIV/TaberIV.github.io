9484afbffa64c6d4afbcecafb84ee38b4cb000e7
Taber McFarlin <tabermcfarlin@gmail.com>
2018-09-18 17:26:49 -0400

    Remove create react app stuff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Taber McFarlin</h1>
         </header>
-        <p className="App-intro">WOOHOO I HAVE A WEBSITE!</p>
+        <p className="App-intro">Site in progress...</p>
       </div>
     );
   }
