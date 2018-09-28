9f8b48109141e74da66969bdc2fd3ff0d411af93
Taber McFarlin <tabermcfarlin@gmail.com>
2018-09-28 17:16:09 -0400

    Links GitHub repo

--- src/ts/components/pages/Home.tsx
+++ src/ts/components/pages/Home.tsx
@@ -5,6 +5,10 @@ const Home = () => (
     <p>
       Welcome to my site! There isn't much yet but feel free too look around!
     </p>
+    <p>
+      The source of this site is hosted on{" "}
+      <a href="https://github.com/TaberIV/TaberIV.github.io">GitHub</a>
+    </p>
   </main>
 );
 
